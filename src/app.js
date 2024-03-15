4f62f7ae5672dddc65c7b83a8c8fa5bce1248435
moviereviewpoint <mrmaryy7777@gmail.com>
2024-03-15 21:42:24 +0530

    removed origin env -v

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.set("trust proxy", true);
