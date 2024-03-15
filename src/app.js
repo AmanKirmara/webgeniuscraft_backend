ca77307764a7ca41f18e908f66d3f90115a2d443
moviereviewpoint <mrmaryy7777@gmail.com>
2024-03-15 22:06:47 +0530

    removed yt resposnse file and env recoverd from dlt

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv"
+dotenv.config()
 
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.set("trust proxy", true);
 
 import userRouter from "./routes/user.routes.js";
 import postBlog from "./routes/blog.routes.js";
-
+userRouter
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/blog", postBlog);
